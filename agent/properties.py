87d3755a5d262ae8863398032710b2d5565f9fd4
larkz <liu.larkin@gmail.com>
2021-12-25 21:11:56 +0100

    isolated and refactored dqn code

---
+++ agent/properties.py
@@ -0,0 +1,56 @@
+import numpy as np
+import torch
+from environment.Gridworld import Gridworld
+from IPython.display import clear_output
+import random
+from matplotlib import pylab as plt
+from collections import deque
+import copy
+
+l1 = 64
+l2 = 150
+l3 = 100
+l4 = 4
+
+mem_size = 1000
+batch_size = 200
+replay = deque(maxlen=mem_size)
+
+epochs = 5000
+losses = [] #A
+
+max_moves = 50
+h = 0
+sync_freq = 500 #A
+j=0
+
+model = torch.nn.Sequential(
+    torch.nn.Linear(l1, l2),
+    torch.nn.ReLU(),
+    torch.nn.Linear(l2, l3),
+    torch.nn.ReLU(),
+    torch.nn.Linear(l3,l4)
+)
+
+model2 = copy.deepcopy(model) #A
+model2.load_state_dict(model.state_dict()) #B|
+
+loss_fn = torch.nn.MSELoss()
+learning_rate = 1e-3
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+gamma = 0.9
+epsilon = 1.0
+learning_rate = 1e-3
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+gamma = 0.9
+epsilon = 1.0
+
+action_set = {
+    0: 'u',
+    1: 'd',
+    2: 'l',
+    3: 'r',
+}
+
---
+++ dqn_script.py
@@ -0,0 +1,85 @@
+import numpy as np
+import torch
+from environment.Gridworld import Gridworld
+from IPython.display import clear_output
+import random
+from matplotlib import pylab as plt
+from collections import deque
+from common.test import *
+
+from agent.properties import *
+
+if torch.cuda.is_available():
+    devid = torch.device('cuda:0')
+else:
+    devid = torch.device('cpu')
+
+for i in range(epochs):
+    game = Gridworld(size=4, mode='random')
+    state1_ = game.board.render_np().reshape(1,64) + np.random.rand(1,64)/100.0
+    state1 = torch.from_numpy(state1_).float()
+    status = 1
+    mov = 0
+    while(status == 1): 
+        j+=1
+        mov += 1
+        qval = model(state1)
+        qval_ = qval.data.numpy()
+        if (random.random() < epsilon):
+            action_ = np.random.randint(0,4)
+        else:
+            action_ = np.argmax(qval_)
+        
+        action = action_set[action_]
+        game.makeMove(action)
+        state2_ = game.board.render_np().reshape(1,64) + np.random.rand(1,64)/100.0
+        state2 = torch.from_numpy(state2_).float()
+        reward = game.reward()
+        done = True if reward > 0 else False
+        exp =  (state1, action_, reward, state2, done)
+        replay.append(exp) #H
+        state1 = state2
+        
+        if len(replay) > batch_size:
+            minibatch = random.sample(replay, batch_size)
+            state1_batch = torch.cat([s1 for (s1,a,r,s2,d) in minibatch])
+            action_batch = torch.tensor([a for (s1,a,r,s2,d) in minibatch]).to(device = devid)
+            reward_batch = torch.tensor([r for (s1,a,r,s2,d) in minibatch]).to(device = devid)
+            state2_batch = torch.cat([s2 for (s1,a,r,s2,d) in minibatch])
+            done_batch = torch.tensor([d for (s1,a,r,s2,d) in minibatch]).type(torch.FloatTensor).to(device = devid)
+            Q1 = model(state1_batch).to(device = devid)
+            with torch.no_grad():
+                Q2 = model2(state2_batch).to(device = devid) #B
+            
+            Y = reward_batch + gamma * ((1-done_batch) * torch.max(Q2,dim=1)[0])
+            X = Q1.gather(dim=1,index=action_batch.long().unsqueeze(dim=1)).squeeze()
+            loss = loss_fn(X, Y.detach())
+            print(i, loss.item())
+            clear_output(wait=True)
+            optimizer.zero_grad()
+            loss.backward()
+            losses.append(loss.item())
+            optimizer.step()
+            
+            if j % sync_freq == 0: #C
+                model2.load_state_dict(model.state_dict())
+        if reward != -1 or mov > max_moves:
+            status = 0
+            mov = 0
+        
+losses = np.array(losses)
+
+plt.figure(figsize=(10,7))
+plt.plot(losses)
+plt.xlabel("Epochs",fontsize=22)
+plt.ylabel("Loss",fontsize=22)
+
+max_games = 1000
+wins = 0
+for i in range(max_games):
+    win = test_model(model, mode='random', display=False)
+    if win:
+        wins += 1
+win_perc = float(wins) / float(max_games)
+print("Games played: {0}, # of wins: {1}".format(max_games,wins))
+print("Win percentage: {}%".format(100.0*win_perc))
