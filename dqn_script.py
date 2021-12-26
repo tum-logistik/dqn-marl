f2a958cdd9739721506e0286b7adb3193faeb1ad
larkz <liu.larkin@gmail.com>
2021-12-26 17:12:05 +0100

    updates

--- dqn_script.py
+++ dqn_script.py
@@ -20,11 +20,11 @@ model2.load_state_dict(DQNModel.model.state_dict())
 for i in range(epochs):
     # marketEnv = MarketEnv()
     # state1_ = marketEnv.reset()
-    
-    game = Gridworld(size=4, mode='random')
+
+    game = Gridworld(size=4, mode='random') # GW
+
     rendered_game_boad_1 = game.board.render_np()
     state1_ = game.board.render_np().reshape(1,64) + np.random.rand(1,64)/100.0
-
     state1 = torch.from_numpy(state1_).float().to(device = devid)
 
     status = 1
