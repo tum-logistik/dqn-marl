f698cece688411d94b3df178dd65bbcea02d637d
larkz <liu.larkin@gmail.com>
2021-12-26 20:32:32 +0100

    update market script

--- dqn_script.py
+++ dqn_script.py
@@ -10,29 +10,25 @@ from environment.MarketEnv import MarketEnv
 from common.properties import *
 from dqn_net import DQNNet
 
-# DQNModel = DQNNet() # Market
-STATE_DIM = 64 # GW
-DQNModel = DQNNet(state_dim = STATE_DIM, output_size = 4) # GW
+STATE_DIM = 2
+ACTION_DIM = 101
+DQNModel = DQNNet() # Market
+# DQNModel = DQNNet(state_dim = STATE_DIM, output_size = ACTION_DIM)
 
 target_net = copy.deepcopy(DQNModel.model)
 target_net.load_state_dict(DQNModel.model.state_dict())
 
 for i in range(epochs):
-    # marketEnv = MarketEnv()
-    # state1_ = marketEnv.reset()
-
-    game = Gridworld(size=4, mode='random') # GW
-
-    rendered_game_boad_1 = game.board.render_np()
-    state1_ = game.board.render_np().reshape(1,64) + np.random.rand(1,64)/100.0
-    state1 = torch.from_numpy(state1_).float().to(device = devid)
-
+    marketEnv = MarketEnv()
+    state1_ = marketEnv.reset()
+    
     status = 1
     mov = 0
 
     while(status == 1): 
         j+=1
         mov += 1
+
         qval = DQNModel(state1)
         
         if not torch.cuda.is_available():
@@ -41,40 +37,21 @@ for i in range(epochs):
             qval_ = qval.data.cpu().numpy()
         
         if (random.random() < epsilon):
-            action_ = np.random.randint(0,4)
+            action_ind = np.random.randint(0,4)
         else:
-            action_ = np.argmax(qval_)
+            action_ind = np.argmax(qval_)
         
         # Execute action and upate state, and get reward + boolTerminal
-        # action = action_
-        # marketEnv.step(action)
-        # state2_, reward, done, info_dic = marketEnv.step(action)
-
-        reward, state2, done = game.step(action_)
-        exp = (state1, action_, reward, state2, done)
+        action = action_ind
+        marketEnv.step(action)
+        state2, reward, done, info_dic = marketEnv.step(action)
+        exp = (state1, action, reward, state2, done)
         
         replay.append(exp) #H
         state1 = state2
         
         if len(replay) > batch_size:
             minibatch = random.sample(replay, batch_size)
-
-            # # Could be replaced with pytorch gather
-            # state1_batch = torch.cat([s1 for (s1,a,r,s2,d) in minibatch]).view(batch_size, STATE_DIM).to(device = devid)
-            # action_batch = torch.tensor([a for (s1,a,r,s2,d) in minibatch]).type(torch.FloatTensor).to(device = devid)
-            # reward_batch = torch.tensor([r for (s1,a,r,s2,d) in minibatch]).type(torch.FloatTensor).to(device = devid)
-            # state2_batch = torch.cat([s2 for (s1,a,r,s2,d) in minibatch]).view(batch_size, STATE_DIM).to(device = devid)
-            # done_batch = torch.tensor([d for (s1,a,r,s2,d) in minibatch]).type(torch.FloatTensor).to(device = devid)
-
-            # # Q update
-            # Q1 = DQNModel(state1_batch).to(device = devid)
-            # with torch.no_grad():
-            #     Q2 = target_net(state2_batch).to(device = devid) #B
-            
-            # Y = reward_batch + gamma * ((1-done_batch) * torch.max(Q2,dim=1)[0])
-            # X = Q1.gather(dim=1,index=action_batch.long().unsqueeze(dim=1)).squeeze()
-            # loss = loss_fn(X, Y.detach())
-
             Q1, Q2, X, Y, loss = DQNModel.batch_update(minibatch, target_net, STATE_DIM)
 
             print(i, loss.item())
@@ -87,23 +64,14 @@ for i in range(epochs):
             
             if j % sync_freq == 0: #C
                 target_net.load_state_dict(DQNModel.model.state_dict())
+
         if done or mov > max_moves:
             status = 0
             mov = 0
-        
+
 losses = np.array(losses)
 
 plt.figure(figsize=(10,7))
 plt.plot(losses)
 plt.xlabel("Epochs",fontsize=22)
 plt.ylabel("Loss",fontsize=22)
-
-# max_games = 1000
-# wins = 0
-# for i in range(max_games):
-#     win = test_model(DQNModel.model, mode='random', display=False)
-#     if win:
-#         wins += 1
-# win_perc = float(wins) / float(max_games)
-# print("Games played: {0}, # of wins: {1}".format(max_games,wins))
-# print("Win percentage: {}%".format(100.0*win_perc))
