bfc9cc32216021a252b3d44408c8b5cb52b9dae1
larkz <liu.larkin@gmail.com>
2021-12-26 23:32:03 +0100

    modular market env

--- environment/MarketEnv.py
+++ environment/MarketEnv.py
@@ -2,16 +2,20 @@ import numpy as np
 
 class MarketEnv():
 
-    def __init__(self, action_size):
+    def __init__(self, action_size, max_demand = 200, demand_slope = 0.5):
         self.max_inventory = 2500
         self.state_env_dim = 2
         
         self.inventory = self.max_inventory
         self.action_space = np.arange(0, action_size)
+        self.action_size = action_size
         self.action_env_dim = len(self.action_space)
 
-        random_price = np.random.uniform(0, 100)
+        random_price = np.random.uniform(0, action_size)
         self.current_state = np.array([self.inventory, random_price])
+
+        self.max_demand = max_demand
+        self.demand_slope = demand_slope
 
     # .seed
     def seed(self, seed):
@@ -21,19 +25,19 @@ class MarketEnv():
 
     # .reset()
     def reset(self):
-        random_ref_price = np.random.uniform(0, 100)
+        random_ref_price = np.random.uniform(0, self.action_size)
         self.inventory = self.max_inventory
         return np.array([self.inventory, random_ref_price])
     
     def get_random_price():
-        return np.random.uniform(0, 100)
+        return np.random.uniform(0, self.action_size)
         
     # .step(action)
     def step(self, action_index):
         set_price = self.action_space[action_index]
         previous_ref_price = self.current_state[1]
 
-        demand_lambda = 200 - 0.5*set_price
+        demand_lambda = self.max_demand - self.demand_slope*set_price
         demand = np.floor(np.random.poisson(demand_lambda))
         
         # reward = ((previous_ref_price + set_price)/2) * demand
