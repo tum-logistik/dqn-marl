234ccac9893ae2414d8e6ef56a379ec395477b8e
larkz <liu.larkin@gmail.com>
2021-12-27 15:18:07 +0100

    change gamma

--- common/properties.py
+++ common/properties.py
@@ -11,11 +11,13 @@ if torch.cuda.is_available():
 else:
     devid = torch.device('cpu')
 
+EPOCHS = 500
+
 mem_size = 500
 batch_size = 50
 replay = deque(maxlen=mem_size)
 
-EPOCHS = 500
+
 losses = [] #A
 
 max_moves = 25
@@ -25,6 +27,6 @@ sync_freq = 40 #A
 loss_fn = torch.nn.MSELoss()
 learning_rate = 1e-3
 
-gamma = 0.95
+GAMMA = 0.8
 epsilon = 0.1
 learning_rate = 1e-3
